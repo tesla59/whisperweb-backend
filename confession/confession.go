3fcde8663cf269bf7a60876b7da673a451234d83
tesla59 <nishant@heim.id>
2023-06-17 14:25:15 +0530

    implement newConfession endpoint

--- confession/confession.go
+++ confession/confession.go
@@ -1,6 +1,8 @@
 package confession
 
 import (
+	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -17,7 +19,13 @@ type Confession struct {
 }
 
 func NewConfession(c *fiber.Ctx) error {
-	return c.SendString("New Confession")
+	db := database.DBConn
+	var confession Confession
+	if err := c.BodyParser(&confession); err != nil {
+		c.Status(http.StatusBadRequest).SendString(fmt.Sprint(err))
+	}
+	db.Create(&confession)
+	return c.Status(http.StatusOK).JSON(confession)
 }
 
 func GetConfession(c *fiber.Ctx) error {
