e79b1ee7be417b278ca7705ac4af18ad2027b5b8
tesla59 <nishant@heim.id>
2023-06-21 15:01:49 +0530

    models: update struct fields with frontend names

--- models/confession.go
+++ models/confession.go
@@ -4,9 +4,9 @@ import "time"
 
 type Confession struct {
 	ID        string    `json:"id"`
-	By        string    `json:"by"`
+	From      string    `json:"from"`
 	To        string    `json:"to"`
-	Text      string    `json:"text"`
+	Message   string    `json:"message"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
